fef3a38fbeeffa8fcdf47bfbd2dcb4461b430af1
zamindar-786 <zamindar900@gmail.com>
2020-07-21 12:15:00 +0530

    Jul 21  12:15:00

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -13,7 +13,4 @@ def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
     a = int(distance)
     if a <= r1-r2:
         return True
-    elif a > r1+r2:
-        return True
-    else:
-        return False
+    return False
