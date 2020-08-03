7ceb79bdd2caf1ec61a356236ce859154bfbdeb4
zamindar-786 <zamindar900@gmail.com>
2020-08-03 11:43:17 +0530

    Aug 03  11:43:17

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -1,11 +1,27 @@
-# Write the function setKthDigit(n, k, d) that takes three integers -- n, k, and d 
-# where n is a possibly-negative int, k is a non-negative int, and d is a non-negative 
-# single digit (between 0 and 9 inclusive). This function returns the number n with 
-# the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
-# so the 0th digit is the rightmost digit. 
-
+# Write the function setKthDigit(n, k, d) that takes three integers -- n, k, and d
+# where n is a possibly-negative int, k is a non-negative int, and d is a non-negative
+# single digit (between 0 and 9 inclusive). This function returns the number n with
+# the kth digit replaced with d. Counting starts at 0 and goes right-to-left,
+# so the 0th digit is the rightmost digit.
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
-
+    if n > 0:
+        num = str(n)
+        if k < len(num):
+            position = int(len(num)-k-1)
+            fold = num[0:position] + str(d) + num[position+1:len(num)]
+            return int(fold)
+        else:
+            fold = str(d) + num
+            return int(fold)
+    else:
+        n = (n) * (-1)
+        num = str(n)
+        if k < len(num):
+            position = int(len(num) - k - 1)
+            fold = num[0:position] + str(d) + digit[position+1:len(num)]
+            return int(fold)
+        else:
+            fold = str(d)+num
+            return int(fold)*(-1)
