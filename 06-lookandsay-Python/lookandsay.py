bf29783650dc23a008248c3408ef8e477d6b8018
zamindar-786 <zamindar900@gmail.com>
2020-08-04 19:08:35 +0530

    Aug 04  19:08:34

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -1,5 +1,5 @@
-# First, you can read about look-and-say numbers here. Then, write the function lookAndSay(a) that takes a list of 
-# numbers and returns a list of numbers that results from "reading off" the initial list using the look-and-say 
+# First, you can read about look-and-say numbers here. Then, write the function lookAndSay(a) that takes a list of
+# numbers and returns a list of numbers that results from "reading off" the initial list using the look-and-say
 # method, using tuples for each (count, value) pair. For example:
 # lookAndSay([]) == []
 # lookAndSay([1,1,1]) == [(3,1)]
@@ -7,6 +7,14 @@
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
+
 def lookandsay(a):
-	# Your code goes here
-	pass
+    total = []
+    first, last, k = 0, 0, 0
+    while k < len(a):
+        while last < len(a) and a[first] == a[last]:
+            last += 1
+        total.append((last - first, a[first]))
+        first = last
+        k = last
+    return total
