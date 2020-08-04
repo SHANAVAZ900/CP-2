079218a17c919cbbb0af1f7b122d91802eeca19c
zamindar-786 <zamindar900@gmail.com>
2020-08-04 11:24:26 +0530

    Aug 04  11:24:26

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -1,6 +1,33 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
+
+
 def quicksort(array):
-	# Your code goes here
-	pass
+    # Your code goes here
+    quick_sort(array, 0, len(array)-1)
+    return array
+
+
+def quick_sort(array, low, high):
+    if low < high:
+        sort_part = seperation(array, low, high)
+        quick_sort(array, low, parted - 1)
+        quick_sort(array, parted+1, high)
+
+
+def seperation(array, low, high):
+    pivot = array[low]
+    start = low + 1
+    end = high
+    while True:
+        while start <= end and array[start] <= pivot:
+            start += 1
+        while start <= end and array[start] >= pivot:
+            end -= 1
+        if start <= end:
+            array[start], array[end] = array[end], array[start]
+        else:
+            break
+    array[low], array[end] = array[end], array[low]
+    return end
