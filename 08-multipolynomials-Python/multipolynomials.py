ca1a55f49b798cf0e3a9b0fe99c27531b8dd59f6
zamindar-786 <zamindar900@gmail.com>
2020-08-04 19:37:09 +0530

    Aug 04  19:37:09

--- 08-multipolynomials-Python/multipolynomials.py
+++ 08-multipolynomials-Python/multipolynomials.py
@@ -1,10 +1,15 @@
-# Background: we can represent a polynomial as a list of its coefficients. For example, [2, 3, 0, 4] could represent 
-# the polynomial 2x3 + 3x2 + 4. With this in mind, write the function multiplyPolynomials(p1, p2) which takes two 
-# lists representing polynomials as just described, and returns a third list representing the polynomial which is 
-# the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x + 
+# Background: we can represent a polynomial as a list of its coefficients. For example, [2, 3, 0, 4] could represent
+# the polynomial 2x3 + 3x2 + 4. With this in mind, write the function multiplyPolynomials(p1, p2) which takes two
+# lists representing polynomials as just described, and returns a third list representing the polynomial which is
+# the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x +
 # 5), and:    (2x**2 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
 # And so this returns the list [8, 10, 12, 15].
 
+
 def multipolynomials(p1, p2):
-	# Your code goes here
-	pass
+    # Your code goes here
+    total = [0] * (len(p1) + len(p2)-1)
+    for j in range(len(p1)):
+        for k in range(len(p2)):
+            total[j + k] += p1[j] + p2[k]
+    return total
