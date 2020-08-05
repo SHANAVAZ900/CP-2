7bce74f24de83707e3a9a86963aa72868b43708a
zamindar-786 <zamindar900@gmail.com>
2020-08-06 00:26:03 +0530

    Aug 06  00:26:03

--- 07-graphrepresentation-Python/Graph_Representation.py
+++ 07-graphrepresentation-Python/Graph_Representation.py
@@ -3,11 +3,13 @@ class Node(object):
         self.value = value
         self.edges = []
 
+
 class Edge(object):
     def __init__(self, value, node_from, node_to):
         self.value = value
         self.node_from = node_from
         self.node_to = node_to
+
 
 class Graph(object):
     def __init__(self, nodes=[], edges=[]):
@@ -17,7 +19,7 @@ class Graph(object):
     def insert_node(self, new_node_val):
         new_node = Node(new_node_val)
         self.nodes.append(new_node)
-        
+
     def insert_edge(self, new_edge_val, node_from_val, node_to_val):
         from_found = None
         to_found = None
@@ -39,13 +41,31 @@ class Graph(object):
 
     def get_edge_list(self):
         edge_list = []
+        for edge in self.edges:
+            edge_list.append(
+                (edge.value, edge.node_from.value, edge.node_to.value))
         return edge_list
 
     def get_adjacency_list(self):
         adjacency_list = [None] * (max_index + 1)
+        rows_columns = self.get_adjacency_matrix()
+        for i in range(len(rows_columns)):
+
+            l = []
+            for j in range(len(rows_columns[i])):
+
+                if rows_columns[i].count(0) != len(self.edges) + 1:
+                    if rows_columns[i][j]:
+                        l.append((j, rows_columns[i][j]))
+                else:
+                    break
+            if l != []:
+                adjacency_list[i] = l
         return adjacency_list
-    
-    
+
     def get_adjacency_matrix(self):
-        adjacency_matrix = [[0 for i in range(max_index + 1)] for j in range(max_index + 1)]
+        adjacency_matrix = [
+            [0 for i in range(max_index + 1)] for j in range(max_index + 1)]
+        for edge in self.edges:
+            adjacency_matrix[edge.node_from.value][edge.node_to.value] = edge.value
         return adjacency_matrix
