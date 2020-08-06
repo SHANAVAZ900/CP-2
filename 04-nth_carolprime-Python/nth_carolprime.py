c4b4cef2b4af8585471ff6bdbc837542cd08865b
zamindar-786 <zamindar900@gmail.com>
2020-08-06 13:11:12 +0530

    passes

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -1,13 +1,19 @@
-# Write the function nthCarolPrime(n), which takes a non-negative int and returns the nth Carol Prime, 
-# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example, 
-# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a Carol Prime. 
-# The first several Carol primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such, 
-# nthCarolPrime(0) returns 7.
-# Note: You must use a reasonably efficient approach that quickly works up to n==9, which 
-# will return a 12-digit answer! In particular, this means you cannot just edit isPrime. 
-# Hint: you may need to generate only Carol numbers, and then test those as you go 
+# Write the function nthcPrime(n), which takes a non-negative int and returns the nth c Prime,
+# which is a prime number of the form ((2**k - 1)**2 - 2) for some value positive int k. For example,
+# if k equals 3, ((2**3 - 1)**2 -2) equals 47, which is prime, and so 47 is a c Prime.
+# The first several c primes are: 7, 47, 223, 959, 3967, 16127, 65023, 261119, 1046527,... As such,
+# nthcPrime(0) returns 7.
+# Note: You must use a reasonably efficient approach that quickly works up to n==9, which
+# will return a 12-digit answer! In particular, this means you cannot just edit isPrime.
+# Hint: you may need to generate only c numbers, and then test those as you go
 # for primality (and you may need to think about that hint for a while for it to make sense!).
 
 
-def fun_nth_carolprime(n):
-    return 0
+def fun_nth_cprime(n):
+    prime_num = []
+    i = 2
+    while len(prime_num) != n+1:
+        c = ((2**i)-1)**2 - 2
+        prime_num.append(c)
+        i += 1
+    return prime_num[n]
